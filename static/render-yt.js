c0b278a46e123796074ad600662cbba052fbcb97
Jeel Shah <jeelshah96@gmail.com>
2016-02-18 18:31:09 -0500

    Added the option to make additional requests

--- static/render-yt.js
+++ static/render-yt.js
@@ -1,6 +1,64 @@
 /*
-We will manipulate a specific DOM elements ... let's see if it works
+    We will manipulate a specific DOM elements ... let's see if it works
+*/
+
+
+/*
+    ouuuu some beautiful javascript
+    Songs listened to while coding (irrelevant but fun to document):
+
+    1. Kanye West - Ultra Light Beam
 
 */
 
-alert('Hello World!!');
+// Variable declarations
+var json_object = '';
+
+function makeCorsRequest(method, url){
+    var xhr = new XMLHttpRequest();
+
+    if("withCredentials" in xhr) {
+        // Thank you Chrome/Firefox/Opera/Safari
+        xhr.open(method, url, true);
+    } else if (typeof XDomainRequest != "undefined"){
+        // boooo, it's IE
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+    } else {
+        // CORS is not supported :(
+        xhr = null;
+    }
+
+    return xhr;
+}
+
+
+function sendCorsRequest(){
+    var url = "http://127.0.0.1:5000/api/";
+
+    alert('inside cors');
+    var xhr = makeCorsRequest('GET', url);
+
+    if (!xhr) {
+        alert('CORS not supported');
+        return;
+    }
+
+    // Let's get that JSON
+    xhr.onload = function() {
+        json_text = xhr.responseText;
+
+        json_object = JSON.parse(json_text).ytvideos;
+
+    };
+
+
+    xhr.onerror = function(){
+        console.log('Whoops, something went wrong whiel making the request');
+    };
+
+    xhr.send();
+
+}
+
+sendCorsRequest();
