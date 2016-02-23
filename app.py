f0c01bb13470c8a87ffb07928ffe655577f0a12e
Jeel shah <jeelshah96@gmail.com>
2016-02-23 11:18:55 -0500

    working post request returns embed code <- untested

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, send_from_directory, request
 from flask.ext.cors import CORS, cross_origin
 
-from renderjs import generate_file
+from renderjs import generate_file, generate_embed
 
 import os
 
@@ -17,11 +17,21 @@ def get_static(filename):
     return send_from_directory(os.path.join(root_dir, 'static'), filename)
 
 
-@app.route('/api/params')
+@app.route('/api/params', methods=['GET', 'POST'])
 @cross_origin()
 def get_params():
+    content = request.get_json(force=True)
+    pageId = content['pageId']
+    youtubeId = content['youtubeId']
+    title = content['title']
+    desc = content['desc']
+    random_number = content['random_number']
 
-    return "get_params"
+    file_path = generate_file(pageId, youtubeId, title, desc, random_number)
+
+    embed_code = generate_embed(file_path)
+
+    return embed_code
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="159.203.108.89", port=8000)
