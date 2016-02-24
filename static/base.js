8c2cab919d77bb7bafd33c177d34b6937119ff86
Jeel shah <jeelshah96@gmail.com>
2016-02-24 00:20:00 -0500

    Updated base.js to include updated id names etc.

--- static/base.js
+++ static/base.js
@@ -63,6 +63,7 @@ var desc = "[desc]";
 var title = "[title]";
 var youtubeID = "[youtubeId]";
 var pageId = "[pageId]";
+var imageId = "[imageId]";
 
 var main_div = document.getElementById(main_div_id);
 
@@ -92,31 +93,74 @@ head.appendChild(style_link);
 */
 
 var main_inner_div = document.createElement("div");
+main_inner_div.setAttribute("id", "main-inner"); 
 
-var main_inner_div_title = document.createElement("div");
-var main_inner_div_title_p = document.createElement("p");
+// Header div start ---
+var main_inner_div_header = document.createElement("div");
+main_inner_div_header.setAttribute("id", "main-inner-header");
+
+var main_inner_div_title_h2 = document.createElement("h2");
+main_inner_div_title_h2.setAttribute("id", "header-title");
+
 var main_inner_div_title_text = document.createTextNode(title);
 
-var main_inner_div_center = document.createElement("div");
+main_inner_div_title_h2.appendChild(main_inner_div_title_text);
+main_inner_div_header.appendChild(main_inner_div_title_h2);
+// Header div end ----
 
-var main_inner_div_desc = document.createElement("div");
+
+// Inner div start ---
+var main_inner_image_div = document.createElement("div");
+main_inner_image_div.setAttribute("id", "main-inner-image");
+
+var main_inner_image = document.createElement("img");
+main_inner_image.setAttribute("width", "100%");
+main_inner_image.setAttribute("src", imageId);
+
+main_inner_image_div.appendChild(main_inner_image);
+// Inner div end ---
+
+// footer div start ---
+var main_inner_div_footer = document.createElement("div");
+main_inner_div_footer.setAttribute("id", "main-inner-footer");
+
 var main_inner_div_desc_p = document.createElement("p");
+main_inner_div_desc_p.setAttribute("id", "desc-p");
+
 var main_inner_div_desc_text = document.createTextNode(desc);
-
-main_inner_div.setAttribute("id","sell0783-main");
-
-main_inner_div_title.setAttribute("id", "sell0783-title");
-main_inner_div_center.setAttribute("id", "sell0783-center");
-main_inner_div_desc.setAttribute("id", "sell0783-desc");
-
-main_inner_div_title_p.appendChild(main_inner_div_title_text);
-main_inner_div_title.appendChild(main_inner_div_title_p);
-
 main_inner_div_desc_p.appendChild(main_inner_div_desc_text);
-main_inner_div_desc.appendChild(main_inner_div_desc_p);
 
-main_inner_div.appendChild(main_inner_div_title);
-main_inner_div.appendChild(main_inner_div_center);
-main_inner_div.appendChild(main_inner_div_desc);
+main_inner_div_footer.appendChild(main_inner_div_desc_p);
+// footer div end ---
 
-main_div.appendChild(main_inner_div);
+// outer footer div start ---
+var main_inner_footer_bottom - document.createElement("div");
+main_inner_footer_bottom.setAttribute("id", "main-inner-footer-bottom");
+
+var sel_inner_ul = document.createElement("ul");
+sel_inner_ul.setAttribute("id", "sel-inner-ul");
+
+var li_one = document.createElement("li");
+var li_two = document.createElement("li");
+var i_heart = document.createElement("i");
+var i_share = document.createElement("i");
+
+i_heart.setAttribute("class", "fa fa-heart");
+i_share.setAttribute("class", "fa fa-share-alt");
+i_share.setAttribute("onclick", "generateEmbed()");
+
+li_one.appendChild(i_heart);
+li_two.appendChild(i_share);
+
+sel_inner_ul.appendChild(li_one);
+sel_inner_ul.appendChild(li_two);
+
+main_inner_footer_bottom.appendChild(sel_inner_ul);
+// outer footer div end ---
+
+
+// Adding all the children to the main div
+main_inner_div.appendChild(main_inner_div_header);
+main_inner_div.appendChild(main_inner_image_div);
+main_inner_div.appendChild(main_inner_div_footer);
+main_inner_div.appendChild(main_inner_footer_bottom);
